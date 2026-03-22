4609144bc98f5a21af5d3587c952a759012478b8
Shivansh Thakur <152285443+not-shivansh@users.noreply.github.com>
2026-03-22 16:14:49 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -88,5 +88,6 @@ def model_meta():
     return jsonify(meta)
 
 if __name__ == '__main__':
-    print(f"\n  Fake News Detector running at: http://127.0.0.1:5000\n")
-    app.run(debug=False, host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    print(f"\n  Fake News Detector running on port {port}\n")
+    app.run(debug=False, host='0.0.0.0', port=port)
